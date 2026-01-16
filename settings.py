cb3663b7652733b8bbf204ddbce816c4c4cd880b
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2026-01-16 11:06:18 +0100

    Refactored settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,13 @@
 """Holds settings for the project."""
 
+import json
 import os
 import pathlib
-import json
 import warnings
+
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from loguru import logger
+from sqlalchemy import Engine, create_engine
 
 warnings.filterwarnings("ignore", category=FutureWarning, module="oemof.solph")
 
@@ -15,35 +17,45 @@ load_dotenv(env_file)
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ROOT_DIR = pathlib.Path(__file__).parent
+CONFIG_DIR = ROOT_DIR / "config"
 DATAPACKAGE_DIR = ROOT_DIR / "datapackages"
 RESULTS_DIR = ROOT_DIR / "results"
 GEOPACKAGES_DIR = ROOT_DIR / "geopackages"
 VIEWS_DIR = ROOT_DIR / "views"
 
+# --- Database Configuration ---
 DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 DB_SCHEMA = os.environ.get("DB_SCHEMA", "resqenergy")
-ENGINE = create_engine(
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
-    connect_args={"options": "-c search_path=resqenergy,public"},
-)
 
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+def connect_to_db(url: str) -> Engine:
+    """Connect to the PostgreSQL database."""
+    logger.info(f"Connecting to database: {url}")
+    return create_engine(
+        DATABASE_URL,
+        connect_args={"options": f"-c search_path={DB_SCHEMA},public"},
+    )
+
+
+ENGINE = connect_to_db(DATABASE_URL)
+
+# --- OEMOF Settings ---
 OEMOF_WRITE_RESULTS = os.getenv("OEMOF_WRITE_RESULTS", "False") == "True"
 OEMOF_SCENARIO = os.getenv("OEMOF_SCENARIO", "dispatch")
 OEMOF_OVERWRITE_RESULTS = os.getenv("OEMOF_OVERWRITE_RESULTS", "False") == "True"
 
-# CLUSTER
-
-CLUSTER_COMPONENT_FILE = ROOT_DIR / "clusters.json"
+# --- Cluster Mapping ---
+CLUSTER_COMPONENT_FILE = CONFIG_DIR / "clusters.json"
 CLUSTER_GEOPACKAGE = GEOPACKAGES_DIR / "clusters.gpkg"
-with CLUSTER_COMPONENT_FILE.open("r", encoding="utf-8") as json_file:
-    CLUSTERS = json.load(json_file)
 
 
-def _create_component_to_cluster_mapping(clusters: dict) -> dict:
+def _create_component_to_cluster_mapping(clusters_data: dict) -> dict:
     """
     Create a mapping from components to their corresponding clusters.
 
@@ -53,7 +65,7 @@ def _create_component_to_cluster_mapping(clusters: dict) -> dict:
     multiple clusters, the function raises an error to prevent ambiguity.
 
     Args:
-        clusters (dict): A dictionary mapping cluster identifiers to lists of components.
+        clusters_data (dict): A dictionary mapping cluster identifiers to lists of components.
 
     Returns:
         dict: A dictionary mapping components to their corresponding cluster identifiers.
@@ -63,7 +75,7 @@ def _create_component_to_cluster_mapping(clusters: dict) -> dict:
 
     """
     component_mapping = {}
-    for cluster, components in clusters.items():
+    for cluster, components in clusters_data.items():
         for component in components:
             if component in component_mapping:
                 raise ValueError(
@@ -73,5 +85,8 @@ def _create_component_to_cluster_mapping(clusters: dict) -> dict:
             component_mapping[component] = cluster
     return component_mapping
 
+
+with CLUSTER_COMPONENT_FILE.open("r", encoding="utf-8") as json_file:
+    CLUSTERS = json.load(json_file)
 
 COMPONENT_CLUSTERS = _create_component_to_cluster_mapping(CLUSTERS)
