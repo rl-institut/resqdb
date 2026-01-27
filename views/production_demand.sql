7b994553a95590f556558ae904970cb8d3b7d409
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2026-01-27 11:56:46 +0100

    Fix storage filtering in production/demand view and add carrier

--- views/production_demand.sql
+++ views/production_demand.sql
@@ -5,11 +5,12 @@ SELECT
     to_node,
     COALESCE(from_label.label, to_label.label) AS label,
     category.category,
+    category.carrier,
     total_energy
 FROM sequence
 JOIN scenario ON sequence.scenario_id = scenario.id
 LEFT JOIN label AS from_label ON sequence.from_node = from_label.component AND NOT from_label.is_bus
 LEFT JOIN label AS to_label ON sequence.to_node = to_label.component AND NOT to_label.is_bus
 LEFT JOIN category USING(from_node, to_node)
-WHERE sequence.attribute = 'flow' and to_node NOT LIKE '%storage%' and from_node NOT LIKE '%conn%' and to_node NOT LIKE '%conn%'
+WHERE sequence.attribute = 'flow' and to_node NOT LIKE ALL(ARRAY['%storage%', '%battery%', '%conn%']) and from_node NOT LIKE ALL(ARRAY['%storage%', '%battery%', '%conn%'])
 ORDER BY scenario_id
