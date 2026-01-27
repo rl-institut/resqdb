796dd0934da9655270ae7f7ab4c45e71675eb93b
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2026-01-27 11:56:01 +0100

    Fix filtering in capacities view

--- views/capacities.sql
+++ views/capacities.sql
@@ -6,7 +6,7 @@ SELECT
 FROM
   scalar
   JOIN scenario ON scalar.scenario_id = scenario.id
-  LEFT JOIN label AS from_label ON scalar.from_node = from_label.component
-  LEFT JOIN label AS to_label ON scalar.to_node = to_label.component
-WHERE attribute = 'capacity' OR attribute = 'total' AND NOT from_label.is_bus AND NOT to_label.is_bus
+  LEFT JOIN label AS from_label ON scalar.from_node = from_label.component AND NOT from_label.is_bus AND from_label.label NOT LIKE 'Abregelung%'
+  LEFT JOIN label AS to_label ON scalar.to_node = to_label.component AND NOT to_label.is_bus
+WHERE (attribute = 'capacity' OR attribute = 'total') AND COALESCE(from_label.label, to_label.label) IS NOT NULL
 GROUP BY scenario_id, scenario.name, COALESCE(from_label.label, to_label.label)
